9249b669116740eb5c4c9a3757b9fdc6e3b1b397
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-05-04 19:20:52 -0400

    Create Menu.java

---
+++ ui/Menu.java
@@ -0,0 +1,37 @@
+// src/ui/Menu.java
+package ui;
+
+import services.CultivoService;
+import java.util.Scanner;
+
+public class Menu {
+    private final CultivoService cultivoService;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public Menu(CultivoService cultivoService) {
+        this.cultivoService = cultivoService;
+    }
+
+    public void mostrarMenu() {
+        boolean salir = false;
+        while (!salir) {
+            System.out.println("\nMenú Principal:");
+            System.out.println("1. Listar cultivos");
+            System.out.println("2. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            String opcion = scanner.nextLine();
+            switch (opcion) {
+                case "1":
+                    cultivoService.getCultivos().forEach(c ->
+                        System.out.println("- " + c.getNombre() + " | " + c.getVariedad() + " | Estado: " + c.getEstado()));
+                    break;
+                case "2":
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        }
+    }
+}
