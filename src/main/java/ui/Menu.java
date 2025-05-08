5a152f9741e837f6abb3ef19da5466699659aaf9
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-05-08 19:51:06 -0400

    Update Menu.java

--- src/main/java/ui/Menu.java
+++ src/main/java/ui/Menu.java
@@ -5,6 +5,7 @@ import services.CultivoService;
 
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Menu {
     private final CultivoService cultivoService;
